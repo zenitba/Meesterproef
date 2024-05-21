0067b1b5cb886e3872fe54e60eb0206698b9f0ef
zenitba <112856019+zenitba@users.noreply.github.com>
2024-05-21 21:02:36 +0200

    net aangespast

--- src/lib/organism/footer.svelte
+++ src/lib/organism/footer.svelte
@@ -29,7 +29,7 @@
       <a href="#home">Araya Elektrotechniek <span>.</span></a>
     </p>
     <p class="copyright">
-      © 2024 <a href="mailto:info@araya-electrotechniek.nl">Araya Electrotechniek</a>. Alle rechten voorbehouden.
+      © 2024 <a href="mailto:info@araya-elektrotechniek.nl">Araya Elektrotechniek</a>. Alle rechten voorbehouden.
     </p>
   </div>
 </footer>
